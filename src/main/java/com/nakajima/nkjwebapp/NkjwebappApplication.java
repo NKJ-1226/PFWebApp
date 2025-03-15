bf8e850beb31743e07a438ce9d2b6f8221ef1054
NKJ-1226 <r.nakajima@oplan.co.jp>
2025-03-15 13:56:08 +0900

    新規作成

---
+++ src/main/java/com/nakajima/nkjwebapp/NkjwebappApplication.java
@@ -0,0 +1,13 @@
+package com.nakajima.nkjwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NkjwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NkjwebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nakajima/nkjwebapp/NkjwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nakajima.nkjwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NkjwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
