4e7f2e7839e810634955166381dc17407d151d77
NKJ-1226 <r.nakajima@oplan.co.jp>
2025-05-25 16:23:55 +0900

    ログイン失敗時の挙動追加

---
+++ src/main/java/com/nakajima/nkjwebapp/controller/LockedController.java
@@ -0,0 +1,14 @@
+package com.nakajima.nkjwebapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LockedController {
+
+    @GetMapping("/locked")
+    public String locked() {
+        return "locked";
+    }
+
+}
